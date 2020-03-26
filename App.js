ab89093a2046e6744bda102d0b3fb0a92534f758
tahere_fahimi <46875632+tahifahimi@users.noreply.github.com>
2020-03-26 14:42:05 +0430

    Add files via upload
    change the structure based on the class, add CSS with flexbox

--- App.js
+++ App.js
@@ -1,54 +1,51 @@
-import React from "react";
-// import styles from "./designCss.css";
-import "./designCss.css";
-import data from "./data.json";
+import React, { Component } from "react";
 import { SocialIcon } from "react-social-icons";
 
-function App() {
-  return (
-    <div className="App">
+import data from "./data.json";
 
-      <div className="navigation">
-        <nav>
-          <ul className="listbar">
-            <li><a href="#home">Home</a></li>
-            <li className="topnav_bar"><a href="#news">News</a></li>
-            <li className="topnav_bar"><a href="#sign">Sign In/up</a></li>
-          </ul>
-        </nav>
-      </div>
+import "./mainCss.css";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="navigation"></div>
 
-      <div className="fullPage">
-        <h1 className="title">{data.title}</h1>
-        <div>
-          {Object.keys(data.link).map(key => {
-            return <SocialIcon url={data.link[key]} />;
-          })}
-          <p>{data.description}</p>
+        <div className="firstSection">
+          <h1 className="title">{data.title}</h1>
+          <div className="icons-wrapper">
+            {Object.keys(data.link).map(key => {
+              return(
+              <div className="icons">
+                <SocialIcon url={data.link[key]} />
+              </div>);
+            })}
+          </div>
+          <p className="texticons">{data.discription}</p>
         </div>
-      </div>
 
-      <div className="fullPage">
-        <div>
-          <h2 className="about">{data.sections[0].title}</h2>
+        <div className="secondSection">
+          <h3>{data.sections[0].title}</h3>
+          <p className="paragraphs">{data.sections[0].items.content}</p>
         </div>
-        <div>
-          <p>{data.sections[0].items[0].content}</p>
-        </div>
-      </div>
 
-      <div className="fullPage">
-        <div>
-          <h2 className="skills">{data.sections[1].title}</h2>
-        </div>
-        <div>
-          <h3>{data.sections[1].items[0].content.discription}</h3>
-          <h3>{data.sections[1].items[1].content.discription}</h3>
-          <h3>{data.sections[1].items[2].content.discription}</h3>
+        <div className="thirdSection">
+          <h3>{data.sections[1].title}</h3>
+          <div className="cards">
+            {data.sections[1].items.map(eachSkill => {
+              return (
+                <div className="logoanddiscription">
+                  <div className="image-wrapper">
+                  <img src={eachSkill.content.image}></img>
+                    </div>
+                  <h4 className="skill-text">{eachSkill.content.discription}</h4>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
